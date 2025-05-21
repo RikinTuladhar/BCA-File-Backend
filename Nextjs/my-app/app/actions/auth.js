a4ef99d0edc92bb6a5d73b01342727053e21f470
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2025-05-21 08:46:20 +0545

    default email and password

--- Nextjs/my-app/app/actions/auth.js
+++ Nextjs/my-app/app/actions/auth.js
@@ -16,23 +16,31 @@ export async function signup(state, formData) {
   }
 
   const { username, password } = validateFields.data;
-  const user = {
-    username,
-    password,
-  };
+  // const user = {
+  //   username,
+  //   password,
+  // };
+
+  const def_admin = "rikinadmin";
+  const def_password = "rikinadmin";
+
 
   try {
-    const res = await axios.post("/api/user", user);
-    console.log(res.data);
-    const { username,role } = res?.data;
-    if (role === "USER") {
-      return {
-        errors: {
-          invalid: ["No User Role Can Enter!!"],
-        },
-      };
+    // const res = await axios.post("/api/user", user);
+    // console.log(res.data);
+    // const { username,role } = res?.data;
+
+    // if (role === "USER") {
+    //   return {
+    //     errors: {
+    //       invalid: ["No User Role Can Enter!!"],
+    //     },
+    //   };
+    // }
+    if(def_admin == username && def_password == password ){
+      
+      await createSession(username)
     }
-    await createSession(username)
    
   } catch (error) {
     console.log(error);
