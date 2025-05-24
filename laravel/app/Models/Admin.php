16b75fdfa0fee1c8e63a8247cb11d884bfcf1646
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2025-05-24 22:00:59 +0545

    Updated

--- laravel/app/Models/Admin.php
+++ laravel/app/Models/Admin.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements FilamentUser
 {
     use Notifiable;
+
+
+    public function canAccessFilament(): bool
+    {
+        return true; // or use role/permission check
+    }
+
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true; // or add your custom logic
+    }
 
     protected $fillable = [
         'name',
