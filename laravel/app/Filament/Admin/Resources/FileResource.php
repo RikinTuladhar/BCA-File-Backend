681e370d4d7d5e9916ea4c5b0653de195c59a598
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2025-05-24 22:43:22 +0545

    Update FileResource.php

--- laravel/app/Filament/Admin/Resources/FileResource.php
+++ laravel/app/Filament/Admin/Resources/FileResource.php
@@ -5,6 +5,8 @@ namespace App\Filament\Admin\Resources;
 use App\Filament\Admin\Resources\FileResource\Pages;
 use App\Filament\Admin\Resources\FileResource\RelationManagers;
 use App\Models\File;
+use App\Models\Semester;
+use App\Models\Subject;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -12,6 +14,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class FileResource extends Resource
 {
@@ -29,12 +32,36 @@ class FileResource extends Resource
             ->schema([
                 Forms\Components\TextInput::make('name')
                     ->required()
+                    ->live(debounce: 3000)
+                    ->afterStateUpdated(function ($state, callable $set) {
+                        $set('slug', Str::slug($state));
+                    })
                     ->maxLength(255),
+
                 Forms\Components\TextInput::make('slug')
                     ->required()
                     ->maxLength(255),
+
+                Forms\Components\Select::make('semester_id')
+                    ->label('Semester')
+                    // ->options(Semester::all()->pluck('name', 'id'))
+                    ->reactive() // make it dynamic
+                    ->afterStateUpdated(fn(callable $set) => $set('subject_id', null))
+                    ->dehydrated(false),
+
                 Forms\Components\Select::make('subject_id')
-                    ->relationship('subject', 'name')
+                    ->label('Subject')
+                    ->options(function (callable $get) {
+                        $semesterId = $get('semester_id');
+                        if (!$semesterId) {
+                            return Subject::pluck('name', 'id'); // or return empty
+                        }
+
+                        return Subject::where('semester_id', $semesterId)
+                            ->pluck('name', 'id');
+                    })
+
+
                     ->required(),
                 Forms\Components\TextInput::make('url')
                     ->required(),
@@ -50,7 +77,7 @@ class FileResource extends Resource
                 Tables\Columns\TextColumn::make('name'),
                 Tables\Columns\TextColumn::make('slug'),
                 Tables\Columns\TextColumn::make('subject.name'),
-                Tables\Columns\TextColumn::make('url'),
+                // Tables\Columns\TextColumn::make('url'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime('d-m-Y'),
                 Tables\Columns\TextColumn::make('updated_at')
