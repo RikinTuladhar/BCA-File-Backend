062f63ead334675760058cb0ec9347cda3cb64b8
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2025-11-02 14:58:49 +0545

    update

--- Nextjs/my-app/lib/db.js
+++ Nextjs/my-app/lib/db.js
@@ -1,18 +1,22 @@
 import mysql from "mysql2/promise";
+
 let connection;
-console.log(process.env.HOST_ENV, process.env.USERNAME, process.env.PASSWORD);
 
 export async function connectToDatabase() {
   if (!connection) {
-    connection = await mysql.createConnection({
-      host: "192.250.229.40", // Accessing host from .env
-      // port: 12083, // Port is hardcoded or can be in .env as well
-      port: 3306, // Port is hardcoded or can be in .env as well
-      user: "etraders_backend_user", // Accessing username from .env
-      password: "HbOD^{hzDB*x6rdj", // Accessing password from .env
-      // database: "defaultdb",
-      database: "etraders_kbc",
-    });
+    try {
+      connection = await mysql.createConnection({
+        host: process.env.HOST_ENV,
+        port: 3306, // default MySQL port
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+      });
+      console.log("✅ Connected to MySQL");
+    } catch (err) {
+      console.error("❌ DB connection failed:", err.message);
+      throw err;
+    }
   }
   return connection;
 }
