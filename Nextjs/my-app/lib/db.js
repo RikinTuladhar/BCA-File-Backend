cf30062cbe97ad81cf761a5b8949fc65b83820e2
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2025-11-02 14:35:41 +0545

    update

--- Nextjs/my-app/lib/db.js
+++ Nextjs/my-app/lib/db.js
@@ -7,9 +7,9 @@ export async function connectToDatabase() {
     connection = await mysql.createConnection({
       host: process.env.HOST_ENV, // Accessing host from .env
       // port: 12083, // Port is hardcoded or can be in .env as well
-      port: 24231, // Port is hardcoded or can be in .env as well
+      port: 3306, // Port is hardcoded or can be in .env as well
       user: "etraders_backend_user", // Accessing username from .env
-      password: process.env.PASSWORD, // Accessing password from .env
+      password: "HbOD^{hzDB*x6rdj", // Accessing password from .env
       // database: "defaultdb",
       database: "etraders_kbc",
     });
